e9cddc06c37b240e0c5b569a68807df83aa74a4c
EllieMcQueen <70542907+EllieMcQueen@users.noreply.github.com>
2020-11-13 09:25:08 -0700

    Checkout
    ExitPass change upeate

--- src/Components/ExitPass.js
+++ src/Components/ExitPass.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { useBarcode } from '@createnextapp/react-barcode';
+
+function ExitPass() {
+  const { inputRef } = useBarcode({
+    value: '1234567890',
+    options: {
+      background: '#ccffff',
+    }
+  });
+  function date(props){
+      const [state, sState] = useState({
+        firstName: '',
+        lastName: "" ,
+        itemTotal: '',
+        quantitiy_purchased: '',
+        date: new Date(),
+
+      })
+
+  }
+  dateObj.setUTCSeconds(secondsValue[ msValue])
 
 
-function ExitPass(props) {
-
-
-
-
-    return(
-        <p>ExitPass</p>
-
-    )
-}
+  return <svg ref={inputRef} />;
+};
 
 export default ExitPass;
+
+
+//date -create
+//total - cartreducer
+// clear cart after user session
+//moount make a state if req.session.user.cart map off of but if its a blank array map off propss
