b37bc2d4d4fd83180b874d56b886e09c000dca69
EllieMcQueen <70542907+EllieMcQueen@users.noreply.github.com>
2020-11-15 17:44:10 -0700

    exit pass

--- src/Components/ExitPass.js
+++ src/Components/ExitPass.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useBarcode } from "@createnextapp/react-barcode";
-
+import '../Style/ExitPass.scss'
 function ExitPass(props) {
   const [state, sState] = useState({
     date: new Date(),
@@ -12,33 +12,33 @@ function ExitPass(props) {
   const { inputRef } = useBarcode({
     value: "1234567890",
     options: {
-    background: "#ccffff",
+    background: "white",
+    
     },
   });
   return (
-    <header className="exit-pass">
+    <header className='exit-body'>
+      
       <div className='exit-head'>
-        <p>Exit Pass</p>
-        <p className='exit-page'>Done</p>
+        <p>Exit Pass </p>
       </div>
-     <div className='barCode'>
-         <p>Exit Pass</p>
-         <span className='date'>date</span>
-         <span className='name'>Ellie McQueen</span>
-         <span className='totalAmount'>total</span>
-         <span className='itemTotal'>item</span>
+       <p className='exit-page'>Done </p>
+    <div className='bar-code-box'>
+         <p className='bar-pass'>Exit Pass </p>
+      <div className='name-date'>
+         <span className='name'>Ellie McQueen </span>
+         <span className='date'>Date </span>
+      </div>
+      <div className='total-item'>
+         <span className='totalAmount'>Total </span>
+         <span className='itemTotal'>item </span>
+      </div>
+      <div className='bar-code'>
       <img ref={inputRef} />
       </div>
+    </div>
+    
     </header>
   );
 }
 export default ExitPass;
-
-//   // const { inputRef } = useBarcode({
-//   //   value: '1234567890',
-//   //   options: {
-//   //     background: '#ccffff',
-//   //   }
-//   // })
-
-//      {/* <svg ref={inputRef} /> */}
